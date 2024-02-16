1f4853210b117bccf730ea53f8a42ae48a4500e6
felipenunes <felipenuneto@gmail.com>
2024-02-16 13:57:43 -0300

    Capturando a ação do teclado para o numero de filhos criar automaticamente as views

--- app/src/main/java/com/example/teste/Teste.java
+++ app/src/main/java/com/example/teste/Teste.java
@@ -1,5 +1,7 @@
 package com.example.teste;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +21,8 @@ public class Teste extends AppCompatActivity{
     private AppCompatEditText qtd_filhos;
     private int child_count;
     private AppCompatButton add;
+
+    private String filhos;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,20 @@ public class Teste extends AppCompatActivity{
         frame = findViewById(R.id.frameLayout);
         qtd_filhos = findViewById(R.id.qtd_filhos);
         add = findViewById(R.id.add);
+        filhos = qtd_filhos.getText().toString();
+        qtd_filhos.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                Toast.makeText(Teste.this, "Key code:"+keyCode+" | KEY EVENT: "+event, Toast.LENGTH_LONG).show();
+                Log.d("KEY CODE","Key code:"+keyCode+" | KEY EVENT: "+event);
+                return false;
+            }
+        });
+        if (!(qtd_filhos.getText().toString()!=null)){
+            add.setText("aaaaaaaaaaaaaaaaaaaaaaa");
+            Toast.makeText(this, "Não tá vazio", Toast.LENGTH_SHORT).show();
+        }
+
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,24 +52,49 @@ public class Teste extends AppCompatActivity{
             }
         });
     }
-    private void addChild(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void addChild() {
         child_count++;
         AppCompatTextView novo_texto = new AppCompatTextView(this);
         LinearLayout linearLayout = new LinearLayout(this);
 
-        View childView = LayoutInflater.from(this).inflate(R.layout.child,null);
+        View childView = LayoutInflater.from(this).inflate(R.layout.child, null);
         int childId = View.generateViewId();
         childView.setId(childId);
         frame.addView(childView);
         AppCompatTextView texto_nome = childView.findViewById(R.id.texto_nome);
         AppCompatTextView texto_idade = childView.findViewById(R.id.texto_idade);
-        String Snome = texto_nome.getText().toString()+childId;
+        String Snome = texto_nome.getText().toString() + childId;
         texto_nome.setText(Snome);
-        String Sidade = texto_idade.getText().toString()+childId;
+        String Sidade = texto_idade.getText().toString() + childId;
         texto_idade.setText(Sidade);
         AppCompatEditText idade = childView.findViewById(R.id.idade);
         AppCompatEditText nome = childView.findViewById(R.id.nome);
-        criarFrames(frame);
+
+    }
+    //criarFrames(frame);
 //        frame.addView(linearLayout);
 //        frame.addView(novo_texto);
 //        novo_texto.setId(child_count);
@@ -71,25 +114,24 @@ public class Teste extends AppCompatActivity{
 //            FrameLayout.LayoutParams.WRAP_CONTENT
 //        );
 //        layoutParams.topMargin = child_count*getResources().getDimensionPixelSize(R.dimen.margin_)
-    }
-    public void criarFrames(View view) {
-        String numFilhosStr = qtd_filhos.getText().toString();
-        if (!numFilhosStr.isEmpty()) {
-            int numFilhos = Integer.parseInt(numFilhosStr);
-            for (int i = 1; i <= numFilhos; i++) {
-                FrameLayout novoFrame = criarFrameParaFilho(i);
-                frame.addView(novoFrame);
-            }
-        }
-    }
-
-    private FrameLayout criarFrameParaFilho(int numeroFilho) {
-        FrameLayout novoFrame = new FrameLayout(this);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        );
-        novoFrame.setLayoutParams(layoutParams);
+//    public void criarFrames(View view) {
+//        String numFilhosStr = qtd_filhos.getText().toString();
+//        if (!numFilhosStr.isEmpty()) {
+//            int numFilhos = Integer.parseInt(numFilhosStr);
+//            for (int i = 1; i <= numFilhos; i++) {
+//                FrameLayout novoFrame = criarFrameParaFilho(i);
+//                frame.addView(novoFrame);
+//            }
+//        }
+//    }
+//
+//    private FrameLayout criarFrameParaFilho(int numeroFilho) {
+//        FrameLayout novoFrame = new FrameLayout(this);
+//        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                LinearLayout.LayoutParams.WRAP_CONTENT
+//        );
+//        novoFrame.setLayoutParams(layoutParams);
 
         //AppCompatEditText nomeEditText = new AppCompatEditText(this);
         //nomeEditText.setHint("Nome do Filho " + numeroFilho);
@@ -101,7 +143,7 @@ public class Teste extends AppCompatActivity{
 
         //novoFrame.addView(nomeEditText);
         //novoFrame.addView(idadeEditText);
-        novoFrame.addView(frame);
-        return novoFrame;
-    }
+//        novoFrame.addView(frame);
+//        return novoFrame;
+//    }
 }
